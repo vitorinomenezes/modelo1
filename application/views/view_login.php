9c8636736808e70302bd5ce3d259ba97e784ac06
VITORINO <vitor_menezes@msn.com>
2017-10-13 18:45:58 -0300

    primeiro commit

---
+++ application/views/view_login.php
@@ -0,0 +1,94 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Modelo 1</title>
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	
+	</style>
+</head>
+<body>
+
+<div style="position:fixed;float:left;top:0px;left:0px;width:100%;height:100px;background-color:#006400;">
+
+<div style="position: relative;float: left;top:10px; left:10px;width:33%; height:70px; background-color:#228B22;border-radius:15px; margin:3px "><label style="font-family:script; font-size: 20px ">
+<label style="position: relative;float:left;width: 99%;top:20px;text-align: center;font-family: arial; font-size:40px; color:#fff"><b>vmSoftwares</b></label>
+</label></div>
+
+<div style="position: relative;float: left;top:10px; left:10px;width:33%; height:70px; background-color:#228B22;border-radius:15px; margin:3px ">
+
+	
+
+</div>
+
+<div style="position: relative;float: left;top:10px; left:10px;width:25%; height:70px; background-color:#228B22;border-radius:15px; margin:3px ">
+	
+	<div style="position: relative;float: left;top:3px; left:10px;width:89%; height:20px; background-color:#666;border-radius:15px; margin:3px;padding:3px ">
+	<img style="position: relative;float:left;top" width="20px" height="20px" src="central/imgs/ico-tel1.png" alt="ico-tel1.png">
+	<label style="position: relative;float:left;top:0px;left:5px;font-family: arial; font-size:14px;color:#fff">Celular : 55 71 9 8820-7487 </label></div>
+
+	<div style="position: relative;float: left;top:3px; left:10px;width:89%; height:20px; background-color:#666;border-radius:15px; margin:3px;padding:3px ">
+	<img style="position: relative;float:left;top" width="20px" height="20px" src="central/imgs/ico-email2.png" alt="ico-tel1.png">
+	<label style="position: relative;float:left;top:0px;left:5px;font-family: arial; font-size:14px;color:#fff">Email : vmsoftwares@vm.com.br </label></div>
+
+</div>
+
+<div style="position: relative;float: left;top:10px; left:25px;width:5.5%; height:70px; background-color:#;border-radius:50px; margin:3px ">
+
+
+	
+	<img style="position: relative;float:left;top:10px; left:5px;border-radius: 30PX" width="60px" height="60px" src="central/imgs/cadeado1.png" alt="ico-tel1.png">
+
+
+
+</div>
+
+
+</div>
+
+</div>
+
+<div style="position:relative;float:left;top:35px;left:-40px;width:106.2%;height:500px;border-bottom:solid;border-width:1px;border-color:#ccc ">
+	
+
+
+
+
+<div style="position: relative;float: left;top:30px; left:5px;width:32%; height:450px;border:dotted;border-width:1px ;border-radius:15px; margin:3px;padding:3px  ">
+	
+</div>
+
+<div style="position: relative;float: left;top:30px; left:10px;width:32%; height:450px;border:dotted;border-width:1px ;border-radius:15px; margin:3px;padding:3px  ">
+	
+</div>
+
+<div style="position: relative;float: left;top:30px; left:10px;width:32%; height:450px;border:dotted;border-width:1px ;border-radius:15px; margin:3px;padding:3px  ">
+	
+</div>
+
+
+
+
+
+
+
+</div>
+
+<div style="position:relative;float:left;top:38px;left:-40px;width:106.2%;height:65px;background-color:#006400">a</div>
+
+</body>
+</html>
